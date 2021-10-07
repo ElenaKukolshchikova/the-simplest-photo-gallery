1e0839dcca3f6ca6b2437036623b275341a44e53
Elena <helen.kuk@yandex.ru>
2021-10-07 19:04:12 +0500

    basic consts

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const basicImage = document.querySelector('.rus-ht');
+const imagesBar = document.querySelector('.img_bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx) // instead of xxx, there will be a path to the picture
+imagesBar.appendChild(newImage);
